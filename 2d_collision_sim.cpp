00bdc8529ce9c4b690d1a87e77defaee8a2d140b
subhash-my-name <subhashbishnoi410@protonmail.com>
2026-08-14 00:43:54 +0530

    minor updates to comments

--- 2d_collision_sim.cpp
+++ 2d_collision_sim.cpp
@@ -27,8 +27,6 @@ void Ball::draw(){
 }
 
 int Ball::get_pos(int limit){
-	Vector2 pos_lim = {screenWidth, screenHeight};
-
 	vector<int> coords(limit);
 	int buffer = 1; // to prevent balls from touching each other upon spawn
 	for(int i = 1; buffer < limit; i++){
@@ -37,12 +35,12 @@ int Ball::get_pos(int limit){
 	}
 	int rnd_ch = rand() % (limit);
 
-	// edit the function such that a used position gets deleted after use.
-	// in order to do so, predefine the coords vector and pass it by reference.
-	// then delete every element that gets used.
-	// this will require two coords vectors: one for x and one for y.
-	// make a separate function that is responsible for assigning each ball[i] position.
-	// that function should call this, pass the coords vector by reference and get a position in return.
+	// this function shall be called by a new function assign_pos().
+	// assign_pos() is responsible for assining each ball[i] a unique position.
+	// that unique position, after getting assigned, should get deleted from the coords vector.
+	// therefore, assign_pos() shall pass two vectors to get_pos(): coords_X and coords_y.
+	// both contain the eligible list of positions for x and y coordinates.
+	// good luck to myself i guess.
 	
 	return coords[rnd_ch];
 }
